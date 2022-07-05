01f0e8404587968e57bf5b366bdc31f7b4d87d3a
Rickard Andersson <dunklas@gmail.com>
2022-07-05 23:03:03 +0200

    First implementation

--- src/parse/docker_base.rs
+++ src/parse/docker_base.rs
@@ -2,25 +2,66 @@ use std::{
     collections::{BTreeSet},
     io
 };
+use serde_json::json;
+use regex::Regex;
+use lazy_static::lazy_static;
 use super::SoupSource;
+use crate::soup::model::{Soup, SoupSourceParseError};
 
 pub struct DockerBase {}
+
+lazy_static! {
+    static ref BASE_PATTERN: Regex = Regex::new(r"^ *FROM +(?P<name>[a-z0-9]+):(?P<version>[a-zA-Z0-9\._-]+) *$").unwrap();
+}
 
 impl<R> SoupSource<R> for DockerBase
 where
     R: io::BufRead,
 {
-    fn soups(reader: R) -> Result<std::collections::BTreeSet<crate::soup::model::Soup>, crate::soup::model::SoupSourceParseError> {
-        Ok(BTreeSet::new())
+    fn soups(reader: R) -> Result<BTreeSet<Soup>, SoupSourceParseError> {
+        Ok(reader.lines()
+            .filter_map(|line| line.ok())
+            .filter(|line| BASE_PATTERN.is_match(&line))
+            .map(|line| {
+                match BASE_PATTERN.captures(&line) {
+                    Some(captures) => {
+                        let name = captures.name("name").unwrap().as_str();
+                        let version = captures.name("version").unwrap().as_str();
+                        Some(Soup{
+                            name: name.to_owned(),
+                            version: version.to_owned(),
+                            meta: json!({})
+                        })
+                    },
+                    None => None
+                }
+            })
+            .filter(|soup| soup.is_some())
+            .map(|soup| soup.unwrap())
+            .collect::<BTreeSet<Soup>>())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::json;
 
-    // https://docs.docker.com/engine/reference/builder/#from
+    #[test]
     fn simple_base() {
+        let content = r#"
+FROM postgres:14.4
+        "#.as_bytes();
 
+        let result = DockerBase::soups(content);
+        assert_eq!(true, result.is_ok());
+        let soups = result.unwrap();
+        let expected_soup = Soup {
+            name: "postgres".to_owned(),
+            version: "14.4".to_owned(),
+            meta: json!({})
+        };
+        assert_eq!(1, soups.len());
+        assert_eq!(true, soups.contains(&expected_soup));
     }
 }
