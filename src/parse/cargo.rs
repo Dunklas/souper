ae286459f4aadc4b84bb63aac60b83b3c82bb316
Rickard Andersson <dunklas@gmail.com>
2022-07-11 01:03:35 +0200

    new test

--- src/parse/cargo.rs
+++ src/parse/cargo.rs
@@ -1,14 +1,14 @@
 use super::SoupParse;
 use crate::soup::model::{Soup, SoupSourceParseError};
-use serde_json::{Map, Value};
 use serde::Deserialize;
-use std::collections::{BTreeSet, HashMap, BTreeMap};
+use serde_json::{Map, Value};
+use std::collections::{BTreeSet, HashMap};
 
 pub struct Cargo {}
 
 #[derive(Deserialize)]
 struct Content {
-    dependencies: HashMap<String, toml::Value>
+    dependencies: HashMap<String, toml::Value>,
 }
 
 impl SoupParse for Cargo {
@@ -20,42 +20,42 @@ impl SoupParse for Cargo {
         let content: Content = match toml::from_str(content) {
             Ok(content) => content,
             Err(e) => {
-                return Err(SoupSourceParseError{
-                    message: format!("Invalid Cargo.toml ({})", e)
+                return Err(SoupSourceParseError {
+                    message: format!("Invalid Cargo.toml ({})", e),
                 });
             }
         };
-        content.dependencies.into_iter()
-            .map(|(dependency, value)| {
-                match value {
-                    toml::Value::String(str) => Ok(Soup {
-                        name: dependency,
-                        version: str.to_owned(),
-                        meta: default_meta.clone()
+        content
+            .dependencies
+            .into_iter()
+            .map(|(dependency, value)| match value {
+                toml::Value::String(str) => Ok(Soup {
+                    name: dependency,
+                    version: str,
+                    meta: default_meta.clone(),
+                }),
+                toml::Value::Table(table) => match table.get("version") {
+                    Some(version) => Ok(Soup {
+                        name: dependency.to_owned(),
+                        version: match version.as_str() {
+                            Some(v) => v.to_owned(),
+                            None => {
+                                return Err(SoupSourceParseError {
+                                    message: format!("Invalid version for: {}", dependency),
+                                });
+                            }
+                        },
+                        meta: default_meta.clone(),
                     }),
-                    toml::Value::Table(table) => match table.get("version") {
-                        Some(version) => Ok(Soup {
-                            name: dependency.to_owned(),
-                            version: match version.as_str() {
-                                Some(v) => v.to_owned(),
-                                None => {
-                                    return Err(SoupSourceParseError{
-                                        message: format!("Invalid version for: {}", dependency)
-                                    });
-                                }
-                            },
-                            meta: default_meta.clone()
-                        }),
-                        None => {
-                            return Err(SoupSourceParseError {
-                                message: format!("Missing version for: {}", dependency)
-                            });
-                        }
-                    },
-                    _ => Err(SoupSourceParseError{
-                        message: format!("Malformed dependency: {}", dependency)
-                    })
-                }
+                    None => {
+                        return Err(SoupSourceParseError {
+                            message: format!("Missing version for: {}", dependency),
+                        });
+                    }
+                },
+                _ => Err(SoupSourceParseError {
+                    message: format!("Malformed dependency: {}", dependency),
+                }),
             })
             .collect::<Result<BTreeSet<Soup>, _>>()
     }
@@ -90,7 +90,7 @@ some-dep = "6.6.6"
 [dependencies]
 serde = { version = "1.0.137", features = ["derive"] }
         "#;
-        let result = Cargo{}.soups(content, &Map::new());
+        let result = Cargo {}.soups(content, &Map::new());
         assert_eq!(true, result.is_ok());
         let soups = result.unwrap();
         assert_eq!(
@@ -101,5 +101,35 @@ serde = { version = "1.0.137", features = ["derive"] }
                 meta: Map::new()
             })
         )
+    }
+
+    #[test]
+    fn multiple_dependencies() {
+        let content = r#"
+[dependencies]
+serde_json = { version = "1.0.82", features = ["preserve_order"] }
+quick-xml = "0.23.0"
+        "#;
+        let result = Cargo {}.soups(content, &Map::new());
+        assert_eq!(true, result.is_ok());
+        let soups = result.unwrap();
+        assert_eq!(2, soups.len());
+        assert_eq!(
+            vec![
+                Soup {
+                    name: "serde_json".to_owned(),
+                    version: "1.0.82".to_owned(),
+                    meta: Map::new()
+                },
+                Soup {
+                    name: "quick-xml".to_owned(),
+                    version: "0.23.0".to_owned(),
+                    meta: Map::new()
+                }
+            ]
+            .into_iter()
+            .collect::<BTreeSet<_>>(),
+            soups
+        );
     }
 }
