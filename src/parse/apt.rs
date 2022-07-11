566ff3aceccdabf2e57ccdeda2d1473cf1b0916c
Rickard Andersson <dunklas@gmail.com>
2022-07-11 10:31:48 +0200

    apt shell

--- src/parse/apt.rs
+++ src/parse/apt.rs
@@ -1,17 +1,17 @@
+use super::SoupParse;
 use crate::soup::model::{Soup, SoupSourceParseError};
 use serde_json::{Map, Value};
 use std::collections::BTreeSet;
 
-pub trait SoupParse {
+
+pub struct Apt {}
+
+impl SoupParse for Apt {
     fn soups(
         &self,
         content: &str,
         default_meta: &Map<String, Value>,
-    ) -> Result<BTreeSet<Soup>, SoupSourceParseError>;
+    ) -> Result<BTreeSet<Soup>, SoupSourceParseError> {
+        todo!()
+    }
 }
-
-pub mod apt;
-pub mod cargo;
-pub mod csproj;
-pub mod docker_base;
-pub mod package_json;
