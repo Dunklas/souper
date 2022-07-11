0592c16f3bff58192d25f981aff642df68e1de5d
Dunklas <dunklas@gmail.com>
2022-07-11 22:49:48 +0200

    Refactor and add tests (#46)

---
+++ src/scan/dir_scan.rs
@@ -0,0 +1,132 @@
+use crate::{
+    parse::{
+        cargo::Cargo, csproj::CsProj, docker_base::DockerBase, package_json::PackageJson, SoupParse,
+    },
+    soup::model::{Soup, SoupContexts, SouperIoError},
+    utils,
+};
+use serde_json::{Map, Value};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    fs,
+    io::Error,
+    path::{Path, PathBuf},
+};
+
+const GLOBAL_EXCLUDE_DIRS: [&str; 3] = ["node_modules", "bin", "obj"];
+
+pub type SoupParsers = Vec<Box<dyn SoupParse>>;
+
+pub fn scan(
+    dir: &PathBuf,
+    exclude_dirs: &Vec<PathBuf>,
+    default_meta: Map<String, Value>,
+) -> Result<SoupContexts, SouperIoError> {
+    let path_parsers = match scan_dirs_recursively(dir, exclude_dirs) {
+        Ok(path_parsers) => path_parsers,
+        Err(e) => {
+            return Err(SouperIoError {
+                message: format!("Error while scanning directory: {}", e),
+            });
+        }
+    };
+    let contexts = path_parsers
+        .into_iter()
+        .map(|(path, parsers)| {
+            let file_content = match fs::read_to_string(&path) {
+                Ok(content) => content,
+                Err(e) => {
+                    return Err(SouperIoError {
+                        message: format!("Not able to read file: {} ({})", path.display(), e),
+                    })
+                }
+            };
+            let parse_results: Result<Vec<_>, _> = parsers
+                .into_iter()
+                .map(|y| y.soups(&file_content, &default_meta))
+                .collect();
+            let soups = match parse_results {
+                Ok(soups_iter) => soups_iter.into_iter().flatten().collect(),
+                Err(e) => {
+                    return Err(SouperIoError {
+                        message: format!("Unable to parse {} due to: {}", path.display(), e),
+                    });
+                }
+            };
+            let context_path = relative_path(&path, dir)?;
+            Ok((context_path, soups))
+        })
+        .collect::<Result<BTreeMap<String, BTreeSet<Soup>>, SouperIoError>>()?;
+    Ok(SoupContexts { contexts })
+}
+
+fn scan_dirs_recursively(
+    root: &PathBuf,
+    exclude_dirs: &Vec<PathBuf>,
+) -> Result<Vec<(PathBuf, SoupParsers)>, Error> {
+    let mut sources: Vec<(PathBuf, Vec<Box<dyn SoupParse>>)> = Vec::new();
+    'entries: for entry in fs::read_dir(root)? {
+        let entry = entry?;
+        let path = entry.path();
+        let file_type = entry.file_type()?;
+        let file_name = entry.file_name();
+        if file_type.is_dir() {
+            for ex in GLOBAL_EXCLUDE_DIRS {
+                if file_name == ex {
+                    continue 'entries;
+                }
+            }
+            for ex in exclude_dirs {
+                if path == *ex {
+                    continue 'entries;
+                }
+            }
+            let mut content = scan_dirs_recursively(&path, exclude_dirs)?;
+            sources.append(&mut content);
+            continue;
+        }
+        if file_type.is_file() {
+            match file_name.to_str() {
+                Some("package.json") => {
+                    sources.push((path, vec![Box::new(PackageJson {})]));
+                }
+                Some("Cargo.toml") => {
+                    sources.push((path, vec![Box::new(Cargo {})]));
+                }
+                Some(file_name_str) if file_name_str.contains(".csproj") => {
+                    sources.push((path, vec![Box::new(CsProj {})]));
+                }
+                Some(file_name_str) if file_name_str.contains("Dockerfile") => {
+                    sources.push((path, vec![Box::new(DockerBase {})]));
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(sources)
+}
+
+fn relative_path<P: AsRef<Path>>(full_path: P, root_path: P) -> Result<String, SouperIoError> {
+    let relative_path = match utils::relative_path(full_path.as_ref(), root_path.as_ref()) {
+        Ok(relative_path) => relative_path,
+        Err(_e) => {
+            return Err(SouperIoError {
+                message: format!(
+                    "Not able to obtain relative path for: {} (from {})",
+                    full_path.as_ref().display(),
+                    root_path.as_ref().display()
+                ),
+            });
+        }
+    };
+    let relative_path = match relative_path.into_os_string().into_string() {
+        Ok(path_string) => path_string,
+        Err(_) => {
+            return Err(SouperIoError {
+                message: "Not able to convert relative path to string".to_string(),
+            })
+        }
+    };
+    let relative_path = relative_path.replace('\\', "/");
+    Ok(relative_path)
+}
--- src/soup/contexts_io.rs
+++ src/soup/contexts_io.rs
@@ -1,91 +1,55 @@
-use serde_json::{Map, Value};
 use std::collections::{BTreeMap, BTreeSet};
 use std::fs;
-use std::io::{BufReader, Write};
-use std::path::{Path, PathBuf};
+use std::io::{BufReader, Read, Write};
+use std::path::PathBuf;
 
-use crate::parse::SoupParse;
 use crate::soup::model::{Soup, SoupContexts, SouperIoError};
-use crate::utils;
 
 impl SoupContexts {
-    pub fn from_paths<P: AsRef<Path>>(
-        paths: Vec<(PathBuf, Vec<Box<dyn SoupParse>>)>,
-        source_dir: P,
-        default_meta: Map<String, Value>,
-    ) -> Result<SoupContexts, SouperIoError> {
-        let mut soup_contexts: BTreeMap<String, BTreeSet<Soup>> = BTreeMap::new();
-        for (path, parsers) in paths {
-            let file_content = match fs::read_to_string(&path) {
-                Ok(content) => content,
-                Err(e) => {
-                    return Err(SouperIoError {
-                        message: format!("Not able to read file: {} ({})", path.display(), e),
-                    })
-                }
-            };
-            let parse_results: Result<Vec<_>, _> = parsers
-                .into_iter()
-                .map(|y| y.soups(&file_content, &default_meta))
-                .collect();
-            let soups = match parse_results {
-                Ok(soups_iter) => soups_iter.into_iter().flatten().collect(),
-                Err(e) => {
-                    return Err(SouperIoError {
-                        message: format!("Unable to parse {} due to: {}", path.display(), e),
-                    });
-                }
-            };
-            let context_path = relative_path(path.as_ref(), source_dir.as_ref())?;
-            soup_contexts.insert(context_path, soups);
-        }
-        Ok(SoupContexts {
-            contexts: soup_contexts,
-        })
-    }
-
-    pub fn from_output_file<P: AsRef<Path>>(file_path: P) -> Result<SoupContexts, SouperIoError> {
-        let output_file = match fs::File::open(file_path.as_ref()) {
+    pub fn read_from_file(file_path: &PathBuf) -> Result<SoupContexts, SouperIoError> {
+        let output_file = match fs::File::open(file_path) {
             Ok(file) => file,
             Err(e) => {
                 return Err(SouperIoError {
-                    message: format!(
-                        "Not able to open file: {} ({})",
-                        file_path.as_ref().display(),
-                        e
-                    ),
+                    message: format!("Not able to open file: {} ({})", file_path.display(), e),
                 });
             }
         };
         let reader = BufReader::new(output_file);
+        SoupContexts::read(reader)
+    }
+
+    fn read<R>(reader: R) -> Result<SoupContexts, SouperIoError>
+    where
+        R: Read,
+    {
         let contexts: BTreeMap<String, BTreeSet<Soup>> = match serde_json::from_reader(reader) {
             Ok(contexts) => contexts,
             Err(e) => {
                 return Err(SouperIoError {
-                    message: format!(
-                        "Not able to parse file: {} ({})",
-                        file_path.as_ref().display(),
-                        e
-                    ),
+                    message: format!("Not able to read output-file: {} ", e),
                 });
             }
         };
         Ok(SoupContexts { contexts })
     }
 
-    pub fn write_to_file<P: AsRef<Path>>(&self, file_path: P) -> Result<(), SouperIoError> {
-        let mut output_file = match fs::File::create(&file_path) {
+    pub fn write_to_file(&self, file_path: &PathBuf) -> Result<(), SouperIoError> {
+        let mut output_file = match fs::File::create(file_path) {
             Ok(file) => file,
             Err(e) => {
                 return Err(SouperIoError {
-                    message: format!(
-                        "Not able to create file: {} ({})",
-                        file_path.as_ref().display(),
-                        e
-                    ),
-                })
+                    message: format!("Not able to create file: {} ({})", file_path.display(), e),
+                });
             }
         };
+        self.write(&mut output_file)
+    }
+
+    fn write<W>(&self, writer: &mut W) -> Result<(), SouperIoError>
+    where
+        W: Write,
+    {
         let json = match serde_json::to_string_pretty(&self.contexts()) {
             Ok(json) => json,
             Err(e) => {
@@ -94,42 +58,143 @@ impl SoupContexts {
                 })
             }
         };
-        match output_file.write_all(json.as_bytes()) {
+        match writer.write_all(json.as_bytes()) {
             Ok(_x) => Ok(()),
             Err(e) => {
                 return Err(SouperIoError {
-                    message: format!(
-                        "Not able to write to file: {} ({})",
-                        file_path.as_ref().display(),
-                        e
-                    ),
+                    message: format!("Not able to write output-file: {}", e),
                 })
             }
         }
     }
 }
 
-fn relative_path<P: AsRef<Path>>(full_path: P, root_path: P) -> Result<String, SouperIoError> {
-    let relative_path = match utils::relative_path(full_path.as_ref(), root_path.as_ref()) {
-        Ok(relative_path) => relative_path,
-        Err(_e) => {
-            return Err(SouperIoError {
-                message: format!(
-                    "Not able to obtain relative path for: {} (from {})",
-                    full_path.as_ref().display(),
-                    root_path.as_ref().display()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::Map;
+
+    #[test]
+    fn read() {
+        let input = r#"
+{
+    "src/package.json": [
+        {
+            "name": "some-dependency",
+            "version": "6.6.6",
+            "meta": {}
+        },
+        {
+            "name": "another-dependency",
+            "version": "42",
+            "meta": {
+                "rationale": "Do this and that"
+            }
+        }
+    ],
+    "src/Dockerfile": [
+        {
+            "name": "some-image",
+            "version": "6.0-jammy",
+            "meta": {}
+        }
+    ]
+}
+        "#;
+        let result = SoupContexts::read(input.as_bytes());
+
+        assert_eq!(true, result.is_ok());
+        let contexts = result.unwrap().contexts;
+        assert_eq!(true, contexts.contains_key("src/package.json"));
+        assert_eq!(
+            vec![
+                Soup {
+                    name: "some-dependency".to_owned(),
+                    version: "6.6.6".to_owned(),
+                    meta: Map::new()
+                },
+                Soup {
+                    name: "another-dependency".to_owned(),
+                    version: "42".to_owned(),
+                    meta: serde_json::json!({ "rationale": "Do this and that" })
+                        .as_object()
+                        .unwrap()
+                        .clone()
+                }
+            ]
+            .into_iter()
+            .collect::<BTreeSet<Soup>>(),
+            *contexts.get("src/package.json").unwrap()
+        );
+        assert_eq!(true, contexts.contains_key("src/Dockerfile"));
+        assert_eq!(
+            vec![Soup {
+                name: "some-image".to_owned(),
+                version: "6.0-jammy".to_owned(),
+                meta: Map::new()
+            }]
+            .into_iter()
+            .collect::<BTreeSet<Soup>>(),
+            *contexts.get("src/Dockerfile").unwrap()
+        )
+    }
+
+    #[test]
+    fn write() {
+        let input = SoupContexts {
+            contexts: vec![
+                (
+                    "src/package.json".to_owned(),
+                    vec![Soup {
+                        name: "some-dependency".to_owned(),
+                        version: "6.6.6".to_owned(),
+                        meta: Map::new(),
+                    }]
+                    .into_iter()
+                    .collect::<BTreeSet<Soup>>(),
                 ),
-            });
-        }
-    };
-    let relative_path = match relative_path.into_os_string().into_string() {
-        Ok(path_string) => path_string,
-        Err(_) => {
-            return Err(SouperIoError {
-                message: "Not able to convert relative path to string".to_string(),
-            })
-        }
-    };
-    let relative_path = relative_path.replace('\\', "/");
-    Ok(relative_path)
+                (
+                    "src/Dockerfile".to_owned(),
+                    vec![Soup {
+                        name: "some-image".to_owned(),
+                        version: "6.0-jammy".to_owned(),
+                        meta: serde_json::json!({"rationale": "Do this and that" })
+                            .as_object()
+                            .unwrap()
+                            .clone(),
+                    }]
+                    .into_iter()
+                    .collect::<BTreeSet<Soup>>(),
+                ),
+            ]
+            .into_iter()
+            .collect::<BTreeMap<String, BTreeSet<Soup>>>(),
+        };
+
+        let mut buffer = Vec::<u8>::new();
+        input.write(&mut buffer).unwrap();
+        assert_eq!(
+            r#"{
+  "src/Dockerfile": [
+    {
+      "name": "some-image",
+      "version": "6.0-jammy",
+      "meta": {
+        "rationale": "Do this and that"
+      }
+    }
+  ],
+  "src/package.json": [
+    {
+      "name": "some-dependency",
+      "version": "6.6.6",
+      "meta": {}
+    }
+  ]
+}"#
+            .to_owned()
+            .trim(),
+            String::from_utf8(buffer).unwrap().trim()
+        );
+    }
 }
