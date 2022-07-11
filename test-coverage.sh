78a2ec701621f3afc6bd8fc52b39f471ddef7307
Rickard Andersson <dunklas@gmail.com>
2022-07-11 09:09:32 +0200

    test coverage check

--- test-coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cargo clean
-RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="souper-%m.profraw" cargo test --tests
-llvm-profdata merge -sparse souper-*.profraw -o souper.profdata
-
-BINARY=$(ls target/debug/deps/ | grep souper | grep -v ".d")
-llvm-cov report --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile souper.profdata --object target/debug/deps/$BINARY
-
