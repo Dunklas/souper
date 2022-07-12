7a09c2274ea3bfba6361c90f64fc4850d446a9d0
Rickard Andersson <dunklas@gmail.com>
2022-07-12 12:00:49 +0200

    apt-get

--- src/parse/apt.rs
+++ src/parse/apt.rs
@@ -7,7 +7,7 @@ use std::collections::BTreeSet;
 
 pub struct Apt {}
 
-static PATTERNS: [&str; 1] = [r"^apt install (?:\-[a-zA-Z\-]{1} )*(?:\-\-[a-zA-Z\-]+ )*(?P<name>[a-zA-Z0-9]+)=(?P<version>[a-zA-Z0-9\.\-_]+)$"];
+static PATTERNS: [&str; 1] = [r"^apt(?:\-get)? install (?:\-[a-zA-Z\-]{1} )*(?:\-\-[a-zA-Z\-]+ )*(?P<name>[a-zA-Z0-9]+)=(?P<version>[a-zA-Z0-9\.\-_]+)$"];
 lazy_static! {
     static ref PATTERN_SET: RegexSet = RegexSet::new(&PATTERNS).unwrap();
     static ref REGEXES: Vec<Regex> = PATTERN_SET
@@ -63,6 +63,10 @@ mod tests {
     #[test_case("apt install -y -q curl=7.81.0-1ubuntu1.3")]
     #[test_case("apt install --asume-yes --quiet curl=7.81.0-1ubuntu1.3")]
     #[test_case("apt install -y --quiet curl=7.81.0-1ubuntu1.3")]
+    #[test_case("apt-get install curl=7.81.0-1ubuntu1.3")]
+    #[test_case("apt-get install -y -q curl=7.81.0-1ubuntu1.3")]
+    #[test_case("apt-get install --asume-yes --quiet curl=7.81.0-1ubuntu1.3")]
+    #[test_case("apt-get install -y --quiet curl=7.81.0-1ubuntu1.3")]
     fn specific_version(input: &str) {
         let result = Apt {}.soups(input, &Map::new());
         assert_eq!(true, result.is_ok());
